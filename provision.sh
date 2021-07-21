92178f29df87bab87e41bccf9406e9d33c92be9e
ueda-takahiro <ueda196@gmail.com>
2021-07-22 08:37:00 +0900

    vagrant composer install

--- provision.sh
+++ provision.sh
@@ -1,3 +1,9 @@
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php
+sudo php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
 sudo yum -y install http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
 sudo yum -y install mysql-community-server
 
