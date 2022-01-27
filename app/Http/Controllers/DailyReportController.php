0ad39e19e2e60832fb8534404cf042a840ea38e5
ueda-takahiro <ueda196@gmail.com>
2022-01-28 00:57:40 +0900

    tmp

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -35,11 +35,12 @@ class DailyReportController extends Controller
 
         $a = 1;
         $b = 2;
-        $bar = 'crass';
+        $bar = 'cantar';
         $foo = 'saccer';
         if ($a === $b) {$foo = $bar ?? $a ?? $b;} elseif ($a > $b) {$variable = $foo ? 'centar' : 'maib';
 
-        }else{return $a+$b  ; }
+        }
+        else{ return $a+$b  ; }
 
 
     }
