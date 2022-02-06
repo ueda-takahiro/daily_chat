8582b17a3781ff26a3cced860406ad544390a4ad
GitHub Action <action@github.com>
2022-02-06 13:59:36 +0000

    fixed by phpcbf

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,8 +8,8 @@ class TestController
     {
         $a = 1;
         $b = 2;
-        if ($a<$b) {
-            $result= new \stdClass();
+        if ($a < $b) {
+            $result = new \stdClass();
             $result->message = '$b is larger than $a.';
             return $result;
         }
